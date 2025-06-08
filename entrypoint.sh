3a4640866e83a7c6a18249475a22ad47b5834fba
Aure1is <git@aure1.is>
2025-06-09 00:24:34 +0200

    1.2.0 - Remove: DB Manager Link and Power by Odoo, Improve Login Screen

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,13 +15,12 @@ mkdir -p /var/lib/odoo
 exec odoo \
     --http-port="${PORT}" \
     --without-demo=True \
-    --db-filter="${ODOO_DATABASE_NAME}" \
+    --db-filter="^${ODOO_DATABASE_NAME}$" \
     --proxy-mode \
     --db_host="${ODOO_DATABASE_HOST}" \
     --db_port="${ODOO_DATABASE_PORT}" \
     --db_user="${ODOO_DATABASE_USER}" \
     --db_password="${ODOO_DATABASE_PASSWORD}" \
-    --database="${ODOO_DATABASE_NAME}" \
     --smtp="${ODOO_SMTP_HOST}" \
     --smtp-port="${ODOO_SMTP_PORT_NUMBER}" \
     --smtp-user="${ODOO_SMTP_USER}" \
